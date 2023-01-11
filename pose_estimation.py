55f5f4230aa2250ba1a4cb28912837d6879540f2
jprocha16 <jprocha@academiafa.edu.pt>
2023-01-11 16:08:01 +0000

    pose_estimation working with sample video

--- pose_estimation.py
+++ pose_estimation.py
@@ -3,7 +3,6 @@ Sample Usage:-
 python pose_estimation.py --K_Matrix calibration_matrix.npy --D_Coeff distortion_coefficients.npy --type DICT_5X5_100
 '''
 
-
 import numpy as np
 import cv2
 import sys
@@ -12,8 +11,7 @@ import argparse
 import time
 
 
-def pose_estimation(frame, aruco_dict_type, matrix_coefficients, distortion_coefficients):
-
+def pose_estimation(frame, aruco_dict_type, matrix_coefficients, distortion_coefficients, arucoid):
     '''
     frame - Frame from the video stream
     matrix_coefficients - Intrinsic matrix of the calibrated camera
@@ -26,37 +24,39 @@ def pose_estimation(frame, aruco_dict_type, matrix_coefficients, distortion_coef
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.aruco_dict = cv2.aruco.Dictionary_get(aruco_dict_type)
     parameters = cv2.aruco.DetectorParameters_create()
-
-
+    tvec = np.zeros((1,1,3))
     # corners, ids, rejected_img_points = cv2.aruco.detectMarkers(gray, cv2.aruco_dict,parameters=parameters,
     #     cameraMatrix=matrix_coefficients,
     #     distCoeff=distortion_coefficients)
     corners, ids, rejected_img_points = cv2.aruco.detectMarkers(gray, cv2.aruco_dict, parameters=parameters)
     cv2.aruco.detectMarkers
 
-        # If markers are detected
+    # If markers are detected
     if len(corners) > 0:
         for i in range(0, len(ids)):
-            # Estimate pose of each marker and return the values rvec and tvec---(different
-            # from those of camera coefficients)
-            rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners[i], 0.02, matrix_coefficients,
-                                                                       distortion_coefficients)
-            print('rotation vector: ', rvec)
-            print('translation vector: ', tvec)
-            # Draw a square around the markers
-            cv2.aruco.drawDetectedMarkers(frame, corners)
-            # Draw Axis
-            # cv2.aruco.drawAxis(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)
-            cv2.drawFrameAxes(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)
-    return frame
+            if ids[i, 0] == arucoid:
+                # Estimate pose of each marker and return the values rvec and tvec---(different
+                # from those of camera coefficients)
+                rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners[i], 0.16, matrix_coefficients,
+                                                                               distortion_coefficients)
+                print('rotation vector: ', rvec)
+                print('translation vector: ', tvec)
+                # Draw a square around the markers
+                cv2.aruco.drawDetectedMarkers(frame, corners)
+                # Draw Axis
+                # cv2.aruco.drawAxis(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)
+                cv2.drawFrameAxes(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)
+    return frame, tvec
+
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument("-k", "--K_Matrix", required=True, help="Path to calibration matrix (numpy file)")
     ap.add_argument("-d", "--D_Coeff", required=True, help="Path to distortion coefficients (numpy file)")
     ap.add_argument("-t", "--type", type=str, default="DICT_ARUCO_ORIGINAL", help="Type of ArUCo tag to detect")
-    ap.add_argument("-i", "--camera", type=bool, required=False, help="Set to True if using webcam")
+    ap.add_argument("-i", "--camera", type=bool, default=False, help="Set to True if using webcam")
     ap.add_argument("-v", "--video", help="Path to the video file")
+    ap.add_argument("-id", "--ident", help="Desired ArUco marker id")
     args = vars(ap.parse_args())
 
     if args["camera"]:
@@ -73,7 +73,6 @@ if __name__ == '__main__':
         if video is None or not video.isOpened():
             print('ERROR: video not found!')
 
-    
     if ARUCO_DICT.get(args["type"], None) is None:
         print(f"ArUCo tag type '{args['type']}' is not supported")
         sys.exit(0)
@@ -83,7 +82,10 @@ if __name__ == '__main__':
     distortion_coefficients_path = args["D_Coeff"]
     k = np.load(calibration_matrix_path)
     d = np.load(distortion_coefficients_path)
+    aruco_id = int(args["ident"])
+
     time.sleep(0.1)
+    cv2.namedWindow('Estimated Pose', cv2.WINDOW_NORMAL)
     while True:
         ret, frame = video.read()
         if not ret:
@@ -93,7 +95,9 @@ if __name__ == '__main__':
         # height = int(width * (h / w))
         #
         # frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_CUBIC)
-        output = pose_estimation(frame, aruco_dict_type, k, d)
+        z_list = []
+        output, t_vec = pose_estimation(frame, aruco_dict_type, k, d, aruco_id)
+        z_list.append(t_vec[0][0][2])
         cv2.imshow('Estimated Pose', output)
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
